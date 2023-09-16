1fe4fdf06b37cee8f25bcfade16c7fab68e030d6
Mahbub2027 <alam201127@gmail.com>
2023-09-17 00:35:10 +0600

    course section added

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -1,11 +1,34 @@
+import PropTypes from 'prop-types';
+import { FaBookOpen, FaDollarSign } from 'react-icons/fa';
 
 
-const Course = () => {
+const Course = ({course}) => {
+    // console.log(course)
+    const {cover_img,title, description, course_price, course_credit} = course;
     return (
-        <div>
-            
+        <div className='shadow-xl m-5 px-3 py-5 w-80 space-y-3'>
+            <img className='w-full' src={cover_img} alt="cover image" />
+            <h2 className='text-lg font-bold'>{title}</h2>
+            <p className='text-sm text-gray-600 '>{description}</p>
+            <div className='flex justify-between'>
+                <div className='flex items-center'>
+                    <p><FaDollarSign></FaDollarSign></p>
+                    <p className='ml-2'>Price: {course_price}</p>
+                </div>
+                <div className='flex items-center'>
+                    {/* <img src={<FaBeer></FaBeer>} alt="" /> */}
+                    <p><FaBookOpen></FaBookOpen></p>
+                    <p className='ml-2'>Credit: {course_credit}hr</p>
+                </div>
+            </div>
+            <div className=''>
+                <button className='text-center p-2 rounded-lg text-white bg-blue-600 w-full'>Select</button>
+            </div>
         </div>
     );
 };
 
+Course.propTypes = {
+    course: PropTypes.object
+}
 export default Course;
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import Course from "../Course/Course";
 
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    useEffect(()=>{
+        fetch('course.json')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+    },[])
+
     return (
         <div className="w-3/4">
-            <h3>Courses</h3>
+            {/* <h3>Courses: {courses.length}</h3> */}
+            <div className='grid grid-cols-3 gap-2'>
+                {
+                    courses.map(course => <Course key={course.id} course={course}></Course>)
+                }
+            </div>
         </div>
     );
 };
