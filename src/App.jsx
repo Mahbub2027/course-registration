b41d06efa3fd9795ce08fac705d05e93c15ae60f
Mahbub2027 <alam201127@gmail.com>
2023-09-16 21:52:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+      <h1 className='text-3xl bg-red-500 text-white font-bold'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
